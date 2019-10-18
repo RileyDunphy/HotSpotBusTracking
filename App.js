50350b2ede8f72cf515c630dc54ad9d5ca0495e2
Joshua Kleine-Deters <josh.kleinedeters@outlook.com>
2019-10-18 13:23:35 -0300

    TitleExample

--- App.js
+++ App.js
@@ -49,6 +49,7 @@ export default class App extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.paragraph}>{text}</Text>
+        <Text>"SomeText"</Text>
       </View>
     );
   }
