44cd6c743f0bc3f48fb47ab85278b88de9680a73
Riley Dunphy <rileydunphy@live.ca>
2019-10-25 14:36:24 -0300

    We're good on the expo

--- App.js
+++ App.js
@@ -1,21 +1,46 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, View, Platform } from 'react-native';
+import {
+  Platform,
+  StyleSheet,
+  Button,
+  TextInput,
+  Text,
+  View,
+  Picker,
+  Alert,
+  AsyncStorage,
+  Geolocation
+} from 'react-native';
+//import AsyncStorage from '@react-native-community/async-storage';
+//import Geolocation from '@react-native-community/geolocation';
+import Constants from 'expo-constants';
 import * as Location from 'expo-location';
 import * as Permissions from 'expo-permissions';
-import Constants from 'expo-constants';
-
 import{TaskManager} from 'expo';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
 
 export default class App extends Component {
   state = {
     location: null,
-    errorMessage: null,
-    id: null,
-
-    
+    timer: setInterval(() => this.findCoordinates(), 5000),
+    txtCityID: null,
+    txtBusID: null,
+    cityID: null,
+    busID: null,
+    //Josh Code
+    time: null,
+    routes: null,
+    routelist: null,
+    selectedRoute: 0,
   };
-
-  componentWillMount() {
+  
+  /*componentWillMount() {
     if (Platform.OS === 'android' && !Constants.isDevice) {
       this.setState({
         errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
@@ -23,9 +48,9 @@ export default class App extends Component {
     } else {
       this._getLocationAsync();
     }
-  }
+  }*/
 
-  _getLocationAsync = async () => {
+  /*_getLocationAsync = async () => {
     let { status } = await Permissions.askAsync(Permissions.LOCATION);
     if (status !== 'granted') {
       this.setState({
@@ -34,38 +59,257 @@ export default class App extends Component {
     }
 
     let location = await Location.getCurrentPositionAsync({});
+    location = JSON.stringify(location);
     this.setState({ location });
+    this.timeConvert();
+          this.publishLocationData();
     setTimeout( this._getLocationAsync, 1000 );
+  };*/
+
+
+  findCoordinates() {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        const location = JSON.stringify(position);
+        this.setState({location});
+        this.timeConvert();
+          this.publishLocationData();
+        
+      },
+      error => Alert.alert(error.message),
+      {enableHighAccuracy: true, timeout: 10000, maximumAge: 0},
+    );
+  }
+  //Author Josh
+  timeConvert() {
+    if (this.location != null) {
+      var temp = JSON.parse(this.state.location); //first parse the json
+
+      var timestamp = temp['timestamp'];
+      var ts = new Date(timestamp);
+      var timeFormatted =
+        ts.getFullYear() +
+        '-' +
+        ts.getMonth() +
+        '-' +
+        ts.getDate() +
+        ' ' +
+        ts.getHours() +
+        ':' +
+        ts.getMinutes() +
+        ':' +
+        ts.getSeconds();
+      this.state.time = timeFormatted;
+    }
+  }
+  //author Josh
+  getCityRoutes = async () => {
+    var city = await AsyncStorage.getItem('cityID');
+    fetch('https://next.hotspotpark.com/busTracking/getRoutesForCity', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({city_id: city}),
+    })
+      .then(response => response.json())
+      .then(data => {
+        this.state.routes = JSON.stringify(data);
+        this.setState({routelist: data});
+      })
+      .catch(error => {
+        console.error(error);
+      });
   };
 
-  render() {
-    let text = 'Waiting..';
-    if (this.state.errorMessage) {
-      text = this.state.errorMessage;
-    } else if (this.state.location) {
-      text = JSON.stringify(this.state.location);
-    }
+  setID = async () => {
+    await AsyncStorage.setItem('cityID', this.txtCityID);
+    await AsyncStorage.setItem('busID', this.txtBusID);
+    this.cityID = await AsyncStorage.getItem('cityID');
+    this.busID = await AsyncStorage.getItem('busID');
+  };
 
-    return (
-      <View style={styles.container}>
-        <Text style={styles.paragraph}>{text}</Text>
-        <Text>"SomeText"</Text>
-      </View>
-    );
+  getID = async () => {
+    this.cityID = await AsyncStorage.getItem('cityID');
+    this.busID = await AsyncStorage.getItem('busID');
+    return this.cityID;
+  };
+
+  clearStorage = async () => {
+    await AsyncStorage.removeItem('cityID');
+    await AsyncStorage.removeItem('busID');
+    this.cityID = await AsyncStorage.getItem('cityID');
+    this.busID = await AsyncStorage.getItem('busID');
+    this.txtCityID = await AsyncStorage.getItem('cityID');
+    this.txtBusID = await AsyncStorage.getItem('busID');
+  };
+
+  checkUserSignedIn() {
+    try {
+      this.getID();
+      let value = this.cityID;
+      //if the value is not null or undefined
+      // (there is something there) then true
+      if (value != undefined && value != null) {
+        return true;
+      }
+      //else storage is empty then false
+      else {
+        return false;
+      }
+    } catch (error) {
+      alert.show('Error getting data');
+    }
+  }
+  //author josh
+  setRoute(routeValue, routeIndex) {
+    this.state.selectedRoute = routeValue;
+  }
+  publishLocationData() {
+    if (this.state.selectedRoute != 0 && this.state.location != null) {
+      temp = JSON.parse(this.state.location);
+      var coords = temp['coords'];
+      var mockedVal = temp['mocked'];
+      var accuracy = coords['accuracy'];
+
+      var altitude = coords['altitude'];
+      var heading = coords['heading'];
+      var lat = coords['latitude'];
+      var lon = coords['longitude'];
+      var speed = coords['speed'];
+      var timestamp = temp['timestamp'];
+      var ts = new Date(timestamp);
+      ts.setMonth(ts.getMonth() + 1);
+
+      var timeFormatted =
+        ts.getFullYear() +
+        '-' +
+        ts.getMonth() +
+        '-' +
+        ts.getDate() +
+        ' ' +
+        ts.getHours() +
+        ':' +
+        ts.getMinutes() +
+        ':' +
+        ts.getSeconds();
+      this.state.time = timeFormatted;
+
+      var json = JSON.stringify({
+        route_id: this.state.selectedRoute,
+        timestamp: timeFormatted,
+        mocked: mockedVal,
+        heading: heading,
+        longitude: lon,
+        latitude: lat,
+        speed: speed,
+        accuracy: accuracy,
+        altitude: altitude,
+      });
+
+      fetch(
+        'https://next.hotspotpark.com/busTracking/submitBusTrackingInformation',
+        {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: json,
+        },
+      ).catch(error => {
+        console.error(error);
+      });
+    }
+  }
+
+  render() {
+    let screen;
+    value = this.checkUserSignedIn();
+    if (this.state.routes == null || this.state.routes == undefined) {
+      this.getCityRoutes(this.state.cityID); //this method sets the routes for the city
+    }
+    //if the cityID is in storage, set the screen to the index
+    if (value) {
+      screen = (
+        <View style={styles.container}>
+          <Text>Location: {this.state.location}</Text>
+          <Text>City ID: {this.cityID}</Text>
+          <Text>Bus ID: {this.busID}</Text>
+          <Text>
+            Selected Bus Route:
+            {this.state.routelist.data[this.state.selectedRoute]}
+          </Text>
+          <Picker
+            selectedValue={this.state.selectedRoute}
+            onValueChange={(itemValue, itemIndex) =>
+              this.setRoute(itemValue, itemIndex)
+            }
+            style={{height: 50, width: 200}}>
+            <Picker.Item label="Select a Route" value="0" />
+            {Object.keys(this.state.routelist.data).map(key => {
+              return (
+                <Picker.Item
+                  label={this.state.routelist.data[key]}
+                  value={key}
+                  key={key}
+                />
+              );
+            })}
+          </Picker>
+          <Button onPress={() => this.clearStorage()} title="Clear Storage" />
+        </View>
+      );
+      //if the cityID is not in storage, set the screen to the login page
+    } else if (!value) {
+      screen = (
+        <View style={styles.container}>
+          <View>
+            <Text>Please enter City ID and Bus ID</Text>
+          </View>
+          <TextInput
+            style={{
+              height: 40,
+              width: 100,
+              borderColor: 'gray',
+              borderWidth: 1,
+            }}
+            onChangeText={text => (this.txtCityID = text)}
+          />
+          <TextInput
+            style={{
+              height: 40,
+              width: 100,
+              borderColor: 'gray',
+              borderWidth: 1,
+            }}
+            onChangeText={text => (this.txtBusID = text)}
+          />
+          <Button onPress={() => this.setID()} title="Sign In" />
+        </View>
+      );
+    }
+    //what it's going to render
+    return <View style={styles.container}>{screen}</View>;
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
     justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ecf0f1',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
   },
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
+  welcome: {
+    fontSize: 20,
     textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
   },
 });
